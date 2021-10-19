79f68238d883232d03fe4d673f2a286509ed4f7d
Josh <jwheeler5@protonmail.com>
2021-10-18 20:35:03 -0400

    added js

---
+++ RoastLog/log.js
@@ -0,0 +1,11 @@
+document.getElementById("addRows").addEventListener("click", function(){
+    console.log("adding rows");
+
+
+});
+
+
+
+// let temp = document.getElementById("roastingTemp").value;
+// let phase = document.getElementById("roastPhase").value;
+// let notes = document.getElementById("roastingNotes").value;
